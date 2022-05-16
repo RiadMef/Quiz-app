c34da829cca0b5c2259b2aa0a704e044b6812190
RiadMefti <104847379+RiadMefti@users.noreply.github.com>
2022-05-15 21:39:21 -0400

    packagae

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import Home from './Pages/Home';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Game from './Pages/Game';
+
 
 const darkTheme = createTheme({
   palette: {
